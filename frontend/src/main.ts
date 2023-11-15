75f9f2c29f45caef9bc4d8e014572ba6e0ef0147
Igor Pivnev <igor.pivnev@kiwitaxi.com>
2023-11-15 11:26:16 +0300

    delete no ts icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import SvgIcon from "vue3-icon";
 import api from "@/entities/api";
 
 import App from "./app";
@@ -11,7 +10,6 @@ import '@/app/assets/index.sass'
 const app = createApp(App);
 app.provide('$api', api)
 
-app.component("svg-icon", SvgIcon as any);
 app.use(createPinia());
 app.use(router);
 
---
+++ frontend/src/pages/Holidays.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, inject } from "vue";
+import { Registration } from "@/widgets/registration";
+// const login = ref("");
+
+const api = inject("$api");
+</script>
+
+<template>
+  <div class="w-80"></div>
+</template>
+
+<style lang="scss">
+.input-text {
+  @apply flex flex-col;
+}
+</style>
